e6b8c946b518c13ef230cf5fc93b8e3f86831132
jyotiprokash <jyotiprokash2000@gmail.com>
2024-08-30 00:28:43 +0600

    task-1 if else

--- condition.js
+++ condition.js
@@ -5,10 +5,46 @@
 //     console.log('number is small')
 // }
 
-var money = 500;
+/* var money = 500;
 if (money > 1000){
     console.log('I will go');
 }
 else {
     console.log('i can not go');
 }
+*/
+
+/* const salary =20000;
+const height = 60;
+const hasCar = false;
+
+if(salary > 30000 || height > 50){
+    console.log('perfact');
+}
+else {
+    console.log('not perfact');
+}
+*/
+
+// const marks = 20;
+// if(marks>=33){
+//     console.log('you are passed');
+// }
+
+// else{
+//     console.log('you are fail');
+// }
+// console.log(6!=6);
+
+// let score = 75;
+// let grade;
+// if (score >= 90) {
+//  grade = "A";
+// } else if (score >= 80) {
+//  grade = "B";
+// }
+// console.log(grade);
+
+let result = 10 < "5";
+console.log(result);
+
---
+++ task.js
@@ -0,0 +1,9 @@
+const burgerPrice = 800;
+const coke = 30;
+
+if(burgerPrice >=500){
+    console.log(burgerPrice)
+}
+else{
+    console.log(burgerPrice + coke);
+}
